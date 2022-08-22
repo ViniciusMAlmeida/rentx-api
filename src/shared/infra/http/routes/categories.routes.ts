fafe02999568497e23814f9b144e30b9e987df23
Vinicius Almeida <vinicius.sec21@gmail.com>
2022-08-22 16:43:08 -0300

    refactoring application, using infra layer

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -1,16 +1,16 @@
 import { Router } from "express";
 import multer from "multer";
 
-import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
-import { ImportCategoryController } from "../modules/cars/useCases/importCategory/ImportCategoryController";
-import { ListCategoriesController } from "../modules/cars/useCases/listCategories/ListCategoriesController";
+import { CreateCategoryController } from "@modules/cars/useCases/createCategory/CreateCategoryController";
+import { ImportCategoryController } from "@modules/cars/useCases/importCategory/ImportCategoryController";
+import { ListCategoriesController } from "@modules/cars/useCases/listCategories/ListCategoriesController";
 
 const categoriesRoutes = Router();
 const upload = multer({ dest: "./tmp" });
 
 const createCategoryController = new CreateCategoryController();
-const importCategoryController = new ImportCategoryController();
 const listCategoriesController = new ListCategoriesController();
+const importCategoryController = new ImportCategoryController();
 
 categoriesRoutes.post("/", createCategoryController.handle);
 
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,10 +3,10 @@ import express from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
+import swaggerFile from "../../../swagger.json";
 import handleExceptions from "./middlewares/handleExceptions";
 import { router } from "./routes";
-import swaggerFile from "./swagger.json";
-import "./database";
+import "@shared/infra/typeorm";
 import "@shared/container";
 
 const app = express();
