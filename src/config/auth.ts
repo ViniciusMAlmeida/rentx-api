7eaadf43266ffaa7c6fed44bce5b37a95692a165
Vinicius Almeida <vinicius.sec21@gmail.com>
2022-09-20 22:51:58 -0300

    add refresh token to AuthenticateUserUseCase

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "7b4cde76d496ad8e3777ec38511b8a8d",
+    expires_in_token: "15min",
+    secret_refresh_token: "b1a6d81b31b190e0aae65981cd5b837a",
+    expires_in_refresh_token: "30d",
+    expire_refresh_token_days: 30,
+};
