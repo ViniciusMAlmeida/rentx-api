c542155125406a26663893821eb763bd7feaa156
Vinicius Almeida <vinicius.sec21@gmail.com>
2022-11-29 12:24:09 -0300

    fix tests

---
+++ src/modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory.ts
@@ -0,0 +1,59 @@
+import { ICreateUserTokenDTO } from "@modules/accounts/dtos/ICreateUserTokenDTO";
+import { UserTokens } from "@modules/accounts/infra/typeorm/entities/UserTokens";
+import { AppError } from "@shared/errors/AppError";
+
+import { IUsersTokensRepository } from "../IUsersTokensRepository";
+
+class UsersTokensRepositoryInMemory implements IUsersTokensRepository {
+    usersTokens: UserTokens[] = [];
+
+    async create({
+        expires_date,
+        refresh_token,
+        user_id,
+    }: ICreateUserTokenDTO): Promise<UserTokens> {
+        const userToken = new UserTokens();
+
+        Object.assign(userToken, {
+            expires_date,
+            refresh_token,
+            user_id,
+        });
+
+        this.usersTokens.push(userToken);
+
+        return userToken;
+    }
+
+    async findByUserIdAndRefreshToken(
+        user_id: string,
+        refresh_token: string
+    ): Promise<UserTokens | undefined> {
+        const userToken = this.usersTokens.find(
+            (ut) => ut.user_id === user_id && ut.refresh_token === refresh_token
+        );
+
+        return userToken;
+    }
+
+    async deleteById(id: string): Promise<void> {
+        const userToken = this.usersTokens.find((ut) => ut.id === id);
+        if (!userToken) {
+            throw new AppError("ID does not exists");
+        }
+
+        this.usersTokens.splice(this.usersTokens.indexOf(userToken));
+    }
+
+    async findByRefreshToken(
+        refresh_token: string
+    ): Promise<UserTokens | undefined> {
+        const userToken = this.usersTokens.find(
+            (ut) => ut.refresh_token === refresh_token
+        );
+
+        return userToken;
+    }
+}
+
+export { UsersTokensRepositoryInMemory };
