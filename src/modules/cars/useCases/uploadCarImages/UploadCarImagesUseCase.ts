32e6016368afd15f81c36922327f632a5c14dc0c
Vinicius Almeida <vinicius.sec21@gmail.com>
2023-02-03 15:39:21 -0300

    add StorageProvider on cars

--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
 import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 import { deleteFile } from "@utils/file";
 
 interface IRequest {
@@ -12,7 +13,9 @@ interface IRequest {
 class UploadCarImagesUseCase {
     constructor(
         @inject("CarsImagesRepository")
-        private carsImagesRepository: ICarsImagesRepository
+        private carsImagesRepository: ICarsImagesRepository,
+        @inject("StorageProvider")
+        private storageProvider: IStorageProvider
     ) {}
     async execute({ car_id, images_name }: IRequest): Promise<void> {
         const imagesExists = await this.carsImagesRepository.find(car_id);
@@ -26,6 +29,7 @@ class UploadCarImagesUseCase {
 
         images_name.forEach(async (image) => {
             await this.carsImagesRepository.create(car_id, image);
+            await this.storageProvider.save(image, "cars");
         });
     }
 }
